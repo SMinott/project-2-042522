7f03f448d7a67e7cfbdcab48709f0d9d822b8e7f
Angelina <angelinarwhite@gmail.com>
2022-06-02 15:26:01 -0400

    vertically center home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ function Home() {
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1 id="homeTitle">Home</h1>
       <div className="homeContainer">
         <img id="homePic" src="https://media1.popsugar-assets.com/files/thumbor/yG7a7_58Bc7D_X5XZWpzeHjY2kg/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2010/05/18/5/192/1922283/mom7/i/Amy-Poehler-Mean-Girls.jpg" />
       </div>
